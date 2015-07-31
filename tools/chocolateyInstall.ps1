2294d28eac59a7591035333daca16898f7768c98
angryhussord <angryhussord@outlook.com>
2015-07-31 15:58:06 -0700

    initial commit

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'backblzae' 
+$installerType = 'exe' 
+$url = 'https://secure.backblaze.com/win32/install_backblaze.exe' 
+$silentArgs = '' 
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
